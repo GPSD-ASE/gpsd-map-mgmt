560486bd37dd7e23293ce047cc41d3338e2de3b7
Rokas Paulauskas <ifamous.rp@gmail.com>
2025-02-22 14:56:35 +0000

    Traffic service work

---
+++ internal/services/traffic_service.go
@@ -0,0 +1,43 @@
+package services
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-resty/resty/v2"
+)
+
+// TrafficServiceInterface defines the methods for retrieving traffic data.
+type TrafficServiceInterface interface {
+	GetTrafficData(lat, lon string) ([]byte, error)
+}
+
+// TrafficService implements TrafficServiceInterface.
+type TrafficService struct {
+	APIKey  string
+	BaseURL string
+}
+
+// NewTrafficService creates a new instance of TrafficService.
+func NewTrafficService(url string, apiKey string) *TrafficService {
+	return &TrafficService{
+		APIKey:  apiKey,
+		BaseURL: url,
+	}
+}
+
+// GetTrafficData fetches traffic data from the TomTom API using provided latitude and longitude.
+func (s *TrafficService) GetTrafficData(lat, lon string) ([]byte, error) {
+	// Build the TomTom API URL.
+	url := fmt.Sprintf("%s?key=%s&point=%s,%s", s.BaseURL, s.APIKey, lat, lon)
+
+	client := resty.New()
+	resp, err := client.R().Get(url)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode() != http.StatusOK {
+		return nil, fmt.Errorf("TomTom API error: %s", resp.Status())
+	}
+	return resp.Body(), nil
+}
