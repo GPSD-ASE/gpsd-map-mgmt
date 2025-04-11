8cbdb39f22dc5ca57e6ce2352d592c41d346c57d
prkaaviya <prkaaviya17@gmail.com>
2025-04-11 18:22:12 +0100

    fix: update bump-version.sh

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -39,6 +39,6 @@ DATE=$(date +%Y-%m-%d)
 awk -v date="$DATE" -v new_ver="$NEW_VERSION" '/## \[Unreleased\]/{print; print ""; print "## [" new_ver "] - " date; next}1' CHANGELOG.md > tmp && mv tmp CHANGELOG.md
 
 echo "NEW_VERSION=$NEW_VERSION"
-echo "new_version=${NEW_VERSION}" >> $GITHUB_ENV
+echo "new_version=${NEW_VERSION}" >> $GITHUB_OUTPUT
 
 echo "Version bumped to $NEW_VERSION"
